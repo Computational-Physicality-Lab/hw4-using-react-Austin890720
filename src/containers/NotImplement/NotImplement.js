05eed7a22b63be5f6957b2fdc223a0a5a4b64da0
austin <as0983228754@gmail.com>
2023-05-02 01:16:36 +0800

    hw4

--- src/containers/NotImplement/NotImplement.js
+++ src/containers/NotImplement/NotImplement.js
@@ -7,7 +7,7 @@ const NotImplement = () => {
     return (
         <Container className='NotImplement'>
             <div className="not_implemented">
-                <img src={scotty} />
+                <img src={scotty} alt=''/>
                     <p className="Oops">Oops, this page doesn't exist yet... how about a shirt from the last page?</p>
             </div>
         </Container >
